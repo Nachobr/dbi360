d920c71ba13adcdf07f717eee9b907ad0884f18a
Nachobr <55702500+Nachobr@users.noreply.github.com>
2021-06-19 00:07:54 -0300

    Update UIInject.js

--- src/Classes/UI/UIInject.js
+++ src/Classes/UI/UIInject.js
@@ -2,11 +2,11 @@ export default class UIInject {
     constructor(parent, wrapper_id) {
         this.app = document.createElement('div')
         this.app.id = wrapper_id
-        
+        this.configInput = this.addConfigInput()
         this.imageInput = this.addImageInput()
         this.viewsInput = this.addViewsInput()
         //Create a New Display (canvas)
-        this.app.appendChild(this.configInput)
+        
         this.app.appendChild(this.imageInput)
         this.app.appendChild(this.viewsInput)
         parent.appendChild(this.app)
